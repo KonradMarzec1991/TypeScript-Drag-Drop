d9000c1f4f172251b267ab7a003216e9aeba1274
Konrad Marzec <konrad.marzec1991@gmail.com>
2022-11-04 14:22:05 +0100

    Add ts configuration in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/apps.ts",
+    mode: "development",
+    entry: "./src/app.ts",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist")
+    },
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
     }
 }
